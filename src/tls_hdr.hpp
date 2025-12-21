960e16a820cae508f09de799d48ffa46ddd535af
cryptogus <wnelem1234@naver.com>
2025-12-21 15:04:15 +0900

    refactor: tls hdr

--- src/tls_hdr.hpp
+++ src/tls_hdr.hpp
@@ -1,12 +1,29 @@
+/**
+ * @file tls_hdr.hpp
+ */
+
 #pragma once
 
 #include <cstdint>
 
-struct TlsHdr
+//----------------------------------
+// Define
+//----------------------------------
+enum TlsType : uint8_t
+{
+    Change_Cipher_Spc = 0x14,
+    Alert,
+    Handshake,
+    Applicatiob_Data
+};
+//----------------------------------
+// Structure
+//----------------------------------
+struct [[gnu::packed]] TlsHdr
 {
     uint8_t type;
     uint8_t ver[2];
     uint8_t len[2];
 };
 
-static_assert(sizeof(TlsHdr) != 5, "TLS Header size is not 5-bytes");
+static_assert(sizeof(TlsHdr) == 5, "TLS Header size is not 5-bytes");

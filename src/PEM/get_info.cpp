8e1a985c66719ee847d1d34db953ad4ba4e62100
cryptogus <wnelem1234@naver.com>
2026-01-03 15:20:48 +0900

    feat: Add taking signature from Certificate

---
+++ src/PEM/get_info.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+namespace PEM
+{
+std::string get_certificate_core(std::istream& is)
+{
+    std::string s, r;
+    while(s != "-----BEGIN") // -----BEGIN CERTIFICATE-----
+    {
+        if (!(is >> s))
+        {
+            return r;
+        }
+    }
+    getline(is, s); // skip the rest of the BEGIN line
+    for (is >> s; s != "-----END"; is >> s) // -----END CERTIFICATE-----
+    {
+        r += s;
+    }
+    return r;
+}
+} // namespace PEM
